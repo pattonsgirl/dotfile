2fe556484da5db42d1d367521e255e1a2c0fb5cf
Kayleigh Duncanwq <pattonsgirl@gmail.com>
2024-03-11 12:44:31 -0400

    adding starter thought

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 if [[ "$(id -u)" -eq 0 ]]; then
 	echo "Script is running as root"
 	# check if apt is package manager
+	# if apt is package manager and you run which apt it will specify a path to where its stored
 		# install packages with apt
 else
 	echo "Script is not running as root, exiting..." 1>&2
