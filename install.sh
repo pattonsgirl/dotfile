1d8759bb4f1264c0db51974e9117453e75ff5179
Kayleigh Duncanwq <pattonsgirl@gmail.com>
2024-03-13 11:33:28 -0400

    can check for apt

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ if [[ "$(id -u)" -eq 0 ]]; then
 	echo "Script is running as root"
 	# check if apt is package manager
 	# if apt is package manager and you run which apt it will specify a path to where its stored
+	echo $(which apt)
+	if [[ -n "$(which apt)" ]]; then
+		echo "apt is installed exactly as specified."
+	else
+		echo "apt is not installed at the specified location."
+	fi
 		# install packages with apt
 else
 	echo "Script is not running as root, exiting..." 1>&2
