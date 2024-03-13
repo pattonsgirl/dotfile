a39ce1aa3ff10eeda7d79c4394d0a966ac38133b
Kayleigh Duncanwq <pattonsgirl@gmail.com>
2024-03-13 13:17:33 -0400

    anaconda done crappily, but it works!

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# get username that called script
+echo $SUDO_USER
+ME=$SUDO_USER
+
 # check if effective user id is 0 (root)
 
 if [[ "$(id -u)" -eq 0 ]]; then
@@ -7,6 +11,29 @@ if [[ "$(id -u)" -eq 0 ]]; then
 	echo $(which apt)
 	if [[ -n "$(which apt)" ]]; then
 		echo "apt is installed exactly as specified."
+		apt install -y \
+			nmap \
+			dnsutils \
+			libgl1-mesa-glx \
+			libegl1-mesa \
+			libxrandr2 \
+			libxrandr2 \
+			libxss1 \
+			libxcursor1 \
+			libxcomposite1 \
+			libasound2 \
+			libxi6 \
+			libxtst6
+		if [[ -e $(ls Anaconda3*.sh 2> /dev/null | head -1) ]]; then
+			echo "Installer found, running it"
+		        bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
+			echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
+	        else
+	 		echo "Downloading anaconda installer"		
+			curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
+			bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
+			echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
+		fi
 	else
 		echo "apt is not installed at the specified location."
 	fi
